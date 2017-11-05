4cc11514c12784cfa475414c2a298c7e93bad524
denispopov <denis@balanceinternet.com.au>
2017-11-05 15:17:01 +0200

    Implemented update user profile

--- resources/views/user/account/user_info.blade.php
+++ resources/views/user/account/user_info.blade.php
@@ -1,5 +1,7 @@
 <div class="margin5">
-    <span><img src="{{ url('/') }}/{{ $user->avatar_path }}" /></span>
+    @if ($user->avatar_path)
+        <span><img src="{{ url('/') }}/{{ $user->avatar_path }}" /></span>
+    @endif
     <span><strong>{{ $user->getFullname() }}</strong></span>
     <span class="pull-right">@include('user.logout')</span>
 </div>
