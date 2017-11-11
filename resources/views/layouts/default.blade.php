b3a2e2671b60895512969f63c91b69fde793a35d
Fedets Maksym <fedets@zfort.com>
2017-11-11 15:45:10 +0200

    fe

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Site</title>
+    <title>Семейный ресурс для хранения фотографий</title>
 
     <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
 
