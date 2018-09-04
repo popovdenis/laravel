183e8d07578d62ccf4298202e792365775b5b270
Maxim Fedets <Fedets@ZFort.local>
2018-09-04 21:30:36 +0300

    added fancybox

--- resources/views/image/list.blade.php
+++ resources/views/image/list.blade.php
@@ -2,18 +2,64 @@
     @foreach ($photos as $key => $photo)
         <div class="gallery-item">
             <div class="gallery-item-photo">
-                <a href="" data-lightbox-target="#photo-item-{{ $photo->id }}" data-lightbox-fit-viewport="false" class="thumbnail">
+                <a href="#photo-item-{{ $photo->id }}" data-fancybox="gallery" class="thumbnail">
                     <img src="{{ url('/') }}/{{ $photo->path_thumb }}"/>
                 </a>
-                <input class="photo-checkbox" type="checkbox" value="{{ $photo->id }}" style="display: none;">
-                <div class="hidden" id="photo-item-{{ $photo->id }}">
+                <div class="photo-item-comments" style="display: none" id="photo-item-{{ $photo->id }}">
                     <img src="{{ url('/') }}/{{ $photo->path }}" alt="">
-                    <div class="gallery-item-comments">
+                    <div class="gallery-item-comments" style="display: none">
                         @include('comments.comments_block', ['photo' => $photo])
                     </div>
                 </div>
+                <input class="photo-checkbox" type="checkbox" value="{{ $photo->id }}" style="display: none;">
             </div>            
         </div>
     @endforeach
 </div>
 
+
+<script>
+
+    // Initialise fancybox with custom settings
+    $('[data-fancybox="gallery"]').fancybox({
+
+        // Disable idle
+        idleTime : 0,
+
+        // Display only these two buttons
+        buttons : [
+            'info', 'close'
+        ],
+
+        // Custom caption
+        caption : function( instance, obj ) {
+            console.log($(this));
+            let commentsForm = $(this).next().find('.gallery-item-comments');
+            return commentsForm.html();
+        },
+
+        onInit: function(instance) {
+
+            // Toggle caption on tap
+            instance.$refs.container.on('touchstart', '[data-fancybox-info]', function(e) {
+                e.stopPropagation();
+                e.preventDefault();
+
+                instance.$refs.container.toggleClass('fancybox-vertical-caption');
+            });
+
+            // Display caption on button hover
+            instance.$refs.container.on('mouseenter', '[data-fancybox-info]', function(e) {
+                instance.$refs.container.addClass('fancybox-vertical-caption');
+
+                // Hide caption when mouse leaves caption area
+                instance.$refs.caption.one('mouseleave', function(e) {
+                    instance.$refs.container.removeClass('fancybox-vertical-caption');
+                });
+
+            });
+
+        }
+
+    });
+</script>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,11 +7,10 @@
 
     <title>Семейный ресурс для хранения фотографий</title>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.4.1/jquery.fancybox.min.css" />
     <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
 
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
-            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-            crossorigin="anonymous"></script>
+    <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="{!! asset('js/bootstrap.js') !!}"></script>
     <link rel="stylesheet" href="{!! asset('css/font-awesome.css') !!}">
     {{--<link rel="stylesheet" href="{!! asset('public/css/style.css') !!}">--}}
@@ -22,6 +21,9 @@
     <script type="text/javascript" src="{!! asset('js/comment-reply.js') !!}"></script>
     <script type="text/javascript" src="{!! asset('js/comment-scripts.js') !!}"></script>
     <script type="text/javascript" src="{!! asset('js/comment.js') !!}"></script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.4.1/jquery.fancybox.min.js"></script>
 </head>
 <body>
 
