af32ffb2d69b56ed077412755de0904c18af8379
denis.popov <denispopov2112@gmail.com>
2025-03-20 21:24:14 +0100

    init

---
+++ src/app/Filament/Resources/RoleResource/Pages/EditRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Resources\RoleResource\Pages;
+
+use App\Filament\Resources\RoleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditRole extends EditRecord
+{
+    protected static string $resource = RoleResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\Action::make('back')
+                ->label('Назад')
+                ->url($this->getResource()::getUrl('index')),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
