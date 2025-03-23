dc86c76c707522e7827150248b3b536e8ee0a4c3
denis.popov <denispopov2112@gmail.com>
2025-03-23 16:49:41 +0100

    Blog

--- src/app/Blog/Models/Comment.php
+++ src/app/Blog/Models/Comment.php
@@ -13,28 +13,9 @@ class Comment extends Model
     ];
 
     public $fillable = [
-
         'comment',
         'author_name',
     ];
-
-
-    /**
-     * The "booting" method of the model.
-     *
-     * @return void
-     */
-    protected static function boot()
-    {
-        parent::boot();
-
-        /* If user is logged in and \Auth::user()->canManageBlogPosts() == true, show any/all posts.
-           otherwise (which will be for most users) it should only show published posts that have a posted_at
-           time <= Carbon::now(). This sets it up: */
-        static::addGlobalScope(new BlogCommentApprovedAndDefaultOrderScope());
-    }
-
-
 
     /**
      * The associated Post
---
+++ src/app/Filament/Resources/CommentResource.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Filament\Resources;
+
+use App\Blog\Models\Comment;
+use App\Filament\Resources\CommentResource\Pages\ListComments;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\Action;
+
+class CommentResource extends Resource
+{
+    protected static ?string $model = Comment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-chat-bubble-left-right';
+    protected static ?string $navigationLabel = 'Comments';
+    protected static ?string $navigationGroup = 'Blog';
+
+    public static function table(Tables\Table $table): Tables\Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('author')->label('Author'),
+                Tables\Columns\TextColumn::make('comment')->limit(80)->label('Comment'),
+                Tables\Columns\TextColumn::make('post.title')->label('Post'),
+                Tables\Columns\TextColumn::make('created_at')->dateTime(),
+                Tables\Columns\IconColumn::make('approved')
+                    ->boolean()
+                    ->label('Approved'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Action::make('approve')
+                    ->visible(fn(Comment $record) => !$record->approved)
+                    ->label('Approve')
+                    ->action(fn(Comment $record) => $record->update(['approved' => true]))
+                    ->requiresConfirmation()
+                    ->color('success'),
+
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListComments::route('/'),
+        ];
+    }
+}
