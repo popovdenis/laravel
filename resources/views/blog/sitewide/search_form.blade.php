81b787b38af9a8d2c298fd8b3f393661fca7ab21
denis.popov <denispopov2112@gmail.com>
2025-04-11 23:10:43 +0200

    Course Schedule

--- resources/views/blog/sitewide/search_form.blade.php
+++ resources/views/blog/sitewide/search_form.blade.php
@@ -1,24 +1,22 @@
-<x-app-layout>
-    <div class="max-w-xl mx-auto my-12 p-6 bg-white dark:bg-gray-800 rounded shadow">
-        <form method="GET" action="{{ route('blog.search', app('request')->get('locale')) }}" class="space-y-4">
-            <h4 class="text-lg font-semibold text-gray-900 dark:text-gray-100 text-center">Search for something in our blog:</h4>
+<div class="max-w-xl mx-auto my-12 p-6 bg-white dark:bg-gray-800 rounded shadow">
+    <form method="GET" action="{{ route('blog.search', app('request')->get('locale')) }}" class="space-y-4">
+        <h4 class="text-lg font-semibold text-gray-900 dark:text-gray-100 text-center">Search for something in our blog:</h4>
 
-            <div>
-                <x-text-input
-                    id="search"
-                    name="s"
-                    type="text"
-                    class="block w-full"
-                    placeholder="Search..."
-                    value="{{ request('s') }}"
-                />
-            </div>
+        <div>
+            <x-text-input
+                id="search"
+                name="s"
+                type="text"
+                class="block w-full"
+                placeholder="Search..."
+                value="{{ request('s') }}"
+            />
+        </div>
 
-            <div class="text-center">
-                <x-primary-button>
-                    {{ __('Search') }}
-                </x-primary-button>
-            </div>
-        </form>
-    </div>
-</x-app-layout>
+        <div class="text-center">
+            <x-primary-button>
+                {{ __('Search') }}
+            </x-primary-button>
+        </div>
+    </form>
+</div>
