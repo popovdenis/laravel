a7f8a9f322124827a8adcf6b380bf73effcf0be9
denis.popov <denispopov2112@gmail.com>
2025-04-13 20:17:43 +0200

    Course Schedule

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
 
 export default defineConfig({
+    server: {
+        https: {
+            key: fs.readFileSync('/Users/denispopov/Sites/laravel/certs/laravel.local-key.pem'),
+            cert: fs.readFileSync('/Users/denispopov/Sites/laravel/certs/laravel.local.pem'),
+        },
+        host: 'laravel.local',
+    },
     plugins: [
         laravel({
             input: [
