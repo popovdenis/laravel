fe76cf2509a3141176ad1872e7bb8ae7cd9c2204
denis.popov <denispopov2112@gmail.com>
2025-04-25 10:21:16 +0200

    Subscriptions

---
+++ app/Filament/Resources/SubscriptionResource/Pages/ViewSubscription.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Filament\Resources\SubscriptionResource\Pages;
+
+use App\Filament\Resources\SubscriptionResource;
+use Filament\Resources\Pages\ViewRecord;
+
+/**
+ * Class ViewSubscription
+ *
+ * @package App\Filament\Resources\SubscriptionResource\Pages
+ */
+class ViewSubscription extends ViewRecord
+{
+    protected static string $resource = SubscriptionResource::class;
+}
