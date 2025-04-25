71cece013f1b3653467449df7e556dfe6b7c2bae
denis.popov <denispopov2112@gmail.com>
2025-04-25 13:18:08 +0200

    Subscriptions

---
+++ app/Listeners/SubscribeUser.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Registered;
+
+/**
+ * Class SubscribeUser
+ *
+ * @package App\Listeners
+ */
+class SubscribeUser
+{
+    /**
+     * Handle the event.
+     */
+    public function handle(Registered $event): void
+    {
+        $event->user->assignRole('Student');
+
+        app(\App\Services\SubscriptionService::class)
+            ->syncSubscriptionForUser($event->user, (int) $event->subscriptionPlanId);
+    }
+}
