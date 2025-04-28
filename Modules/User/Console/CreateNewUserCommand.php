e4756f93fe3753e197056e61eeebdb7e7682abc1
denis.popov <denispopov2112@gmail.com>
2025-04-28 15:00:48 +0200

    Booking & Credits

---
+++ Modules/User/Console/CreateNewUserCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Modules\User\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Modules\User\Models\User;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class CreateNewUserCommand extends Command
+{
+    protected $signature = 'user:create
+                            {name : The name of the user}
+                            {email : The email of the user}
+                            {password? : The password}
+                            {role? : The roles of the user (optional)}';
+
+    protected $description = 'Create a new user';
+
+    public function handle(): int
+    {
+        $name = $this->argument('name');
+        $email = $this->argument('email');
+        $password = $this->argument('password');
+        $role = $this->argument('role');
+
+        if (User::where('email', $email)->exists()) {
+            $this->error('User with email ' . $email . ' already exists.');
+            return self::FAILURE;
+        }
+
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        if ($role) {
+            $user->assignRole($role);
+            $this->info("Role '{$role}' has been assigned to the user.");
+        }
+
+        $this->info("User {$name} with email <{$email}> has been created.");
+
+        return self::SUCCESS;
+    }
+
+    /**
+     * Get the console command arguments.
+     */
+    protected function getArguments(): array
+    {
+        return [
+            ['example', InputArgument::REQUIRED, 'An example argument.'],
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     */
+    protected function getOptions(): array
+    {
+        return [
+            ['example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null],
+        ];
+    }
+}
