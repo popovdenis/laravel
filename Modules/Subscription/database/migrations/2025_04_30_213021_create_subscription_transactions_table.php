93e823384dc335d33c51d0a8b1d66988c0ebd0d2
denis.popov <denispopov2112@gmail.com>
2025-05-03 01:57:23 +0200

    Invoices & Orders

--- Modules/Subscription/database/migrations/2025_04_30_213021_create_subscription_transactions_table.php
+++ Modules/Subscription/database/migrations/2025_04_30_213021_create_subscription_transactions_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('plan_id')->constrained('subscription_plans')->onDelete('cascade');
+            $table->foreignId('plan_id')->nullable()->constrained('subscription_plans')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('type');
-            $table->integer('credits_amount');
+            $table->integer('credits_amount')->nullable();
             $table->enum('action', ['subscription', 'manual', 'promo', 'adjustment']);
             $table->boolean('canceled_immediately')->default(false);
             $table->timestamp('trial_ends_at')->nullable();
