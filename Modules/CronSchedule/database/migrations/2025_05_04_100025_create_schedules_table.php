efa7ea5e2fa7a91e5137a048d005e2ae8090820b
denis.popov <denispopov2112@gmail.com>
2025-05-05 09:21:06 +0200

    Invoices & Orders

--- Modules/CronSchedule/database/migrations/2025_05_04_100025_create_schedules_table.php
+++ Modules/CronSchedule/database/migrations/2025_05_04_100025_create_schedules_table.php
@@ -11,20 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-//        Schema::create('cron_schedules', function (Blueprint $table) {
-//            $table->id();
-//            $table->boolean('enabled')->default(true);
-//            $table->string('target_type')->index();
-//            $table->string('command')->index();
-//            $table->enum('frequency', ['every_minute', 'hourly', 'daily', 'weekly', 'monthly'])->index();
-//            $table->tinyInteger('day')->nullable();
-//            $table->tinyInteger('day_of_week')->nullable();
-//            $table->tinyInteger('hours')->nullable();
-//            $table->tinyInteger('minutes')->nullable();
-//            $table->string('description')->nullable();
-//
-//            $table->timestamps();
-//        });
+        Schema::create('cron_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('enabled')->default(true);
+            $table->string('target_type')->index();
+            $table->string('command')->index();
+            $table->enum('frequency', ['every_minute', 'hourly', 'daily', 'weekly', 'monthly'])->index();
+            $table->tinyInteger('day')->nullable();
+            $table->tinyInteger('day_of_week')->nullable();
+            $table->tinyInteger('hours')->nullable();
+            $table->tinyInteger('minutes')->nullable();
+            $table->string('description')->nullable();
+
+            $table->timestamps();
+        });
     }
 
     /**
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-//        Schema::dropIfExists('cron_schedules');
+        Schema::dropIfExists('cron_schedules');
     }
 };
