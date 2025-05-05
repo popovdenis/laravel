72c23e92abb9ff69e1f8f98b98eb32f9e736328b
denis.popov <denispopov2112@gmail.com>
2025-05-05 15:28:25 +0200

    Invoices & Orders

---
+++ Modules/User/resources/views/profile/partials/account-information.blade.php
@@ -0,0 +1,48 @@
+<div class="bg-gray-100 py-6 px-4">
+    <h2 class="text-xl font-bold inline-block mb-4">
+        {{ __('Account Information') }}
+    </h2>
+    <div class="flex flex-wrap justify-between bg-white rounded-2xl px-4 py-6 lg:px-6 mb-6 lg:mb-10">
+        <div class="w-full lg:w-1/2 md:mb-6 lg:mb-0 lg:pr-4">
+            <div class="flex flex-col h-full sm:flex-row">
+                <div class="grow flex flex-col md:justify-between">
+                    <div>
+                        <h5 class="mb-4 font-bold">{{ __('Customer Subscription Plan') }}</h5>
+                        @if($subscriptionPlan && $subscriptionPlan->name)
+                            <p class="mt-1 text-green-600 font-extrabold text-xl">{{ $subscriptionPlan->name }}</p>
+                        @else
+                            <p>{{ __('You aren\'t subscribed to our newsletter.') }}</p>
+                        @endif
+                    </div>
+                    <a href="{{ route('subscription::show') }}"
+                       class="inline-flex items-center w-full mt-4 text-xm font-semibold hover:underline"
+                       aria-label="Edit newsletters"
+                    >
+                        <span>{{ __('Edit') }}</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="w-full lg:w-1/2">
+            <div class="flex flex-col h-full sm:flex-row">
+                <div class="grow flex flex-col md:justify-between">
+                    <div>
+                        <h5 class="mb-4 font-bold">
+                            <span>{{ __('Contact Information') }}</span>
+                        </h5>
+                        <p>
+                            {{ $user->firstname }} {{ $user->lastname }}<br>
+                            {{ $user->email }}<br>
+                        </p>
+                    </div>
+                    <a href="{{ route('profile.account-information.edit') }}"
+                       class="inline-flex items-center w-full mt-4 text-xm font-semibold hover:underline"
+                       aria-label="Edit contact information"
+                    >
+                        <span>{{ __('Edit') }}</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ Modules/User/resources/views/profile/partials/my-credits.blade.php
@@ -0,0 +1,24 @@
+<div class="bg-gray-100 py-6 px-4">
+    <h2 class="text-xl font-bold inline-block mb-4">
+        {{ __('My Credits') }}
+    </h2>
+
+    <div class="bg-white p-6 rounded shadow-sm flex items-center gap-6">
+        <div class="text-gray-500">
+            <svg class="inline-block ml-3 mr-3 w-12 h-12 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+        </div>
+
+        <div>
+            <div class="text-green-600 font-bold text-3xl">
+                {{ __(':credits credits', ['credits' => $user->credit_balance]) }}
+            </div>
+            <div class="font-semibold mt-1">{{ __('Current Balance')  }}</div>
+            <p class="text-sm text-gray-600 mt-1">
+                Your Credits may expire if there hasn’t been any transactions in the last 24 months.
+            </p>
+        </div>
+    </div>
+</div>
