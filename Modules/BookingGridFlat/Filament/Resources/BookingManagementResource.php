4738e7f55e1a0d62fc921b526bd1d36a5aac6fee
denis.popov <denispopov2112@gmail.com>
2025-05-08 15:21:14 +0200

    Booking

--- Modules/BookingGridFlat/Filament/Resources/BookingManagementResource.php
+++ Modules/BookingGridFlat/Filament/Resources/BookingManagementResource.php
@@ -2,17 +2,20 @@
 
 namespace Modules\BookingGridFlat\Filament\Resources;
 
-use Modules\Booking\Enums\BookingAction;
+use Filament\Tables\Enums\FiltersLayout;
+use Illuminate\Support\Facades\DB;
 use Modules\Booking\Enums\BookingStatus;
 use Modules\BookingGridFlat\Filament\Resources\BookingManagementResource\Pages\CreateBookingManagement;
 use Modules\BookingGridFlat\Filament\Resources\BookingManagementResource\Pages\EditBookingManagement;
 use Modules\BookingGridFlat\Filament\Resources\BookingManagementResource\Pages\ListBookingManagement;
 use Modules\BookingGridFlat\Filament\Resources\BookingManagementResource\RelationManagers;
+use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Modules\BookingGridFlat\Models\BookingGridFlat;
+use Illuminate\Database\Eloquent\Builder;
 
 class BookingManagementResource extends Resource
 {
@@ -63,7 +66,56 @@ class BookingManagementResource extends Resource
                     ->sortable(),
             ])
             ->filters([
-                //
+                Tables\Filters\Filter::make('start_date')->form([
+                    Forms\Components\DatePicker::make('start_date')
+                        ->label('Started At')
+                        ->native(false)
+                        ->minDate(now()),
+                    Forms\Components\DatePicker::make('end_date')
+                        ->label('Ended At')
+                        ->native(false)
+                        ->minDate(now()),
+                    ])
+                    ->query(function (Builder $query, array $data) {
+                        return $query
+                            ->when($data['start_date'], fn ($q) => $q->whereDate('start_time', '>=', $data['start_date']))
+                            ->when($data['end_date'], fn ($q) => $q->whereDate('start_time', '<=', $data['end_date']));
+                    }),
+                Tables\Filters\SelectFilter::make('teacher_id')
+                    ->label('Teacher')
+                    ->options(
+                        BookingGridFlat::select('teacher_id', DB::raw('MIN(teacher_fullname) as teacher_fullname'))
+                            ->groupBy('teacher_id')
+                            ->orderBy('teacher_fullname')
+                            ->pluck('teacher_fullname', 'teacher_id')
+                            ->toArray()
+                    ),
+                Tables\Filters\SelectFilter::make('student_id')
+                    ->label('Student')
+                    ->options(
+                        BookingGridFlat::select('student_id', DB::raw('MIN(student_fullname) as student_fullname'))
+                            ->groupBy('student_id')
+                            ->orderBy('student_fullname')
+                            ->pluck('student_fullname', 'student_id')
+                            ->toArray()
+                    ),
+
+                Tables\Filters\SelectFilter::make('student_id')
+                    ->label('Student')
+                    ->options(
+                        BookingGridFlat::select('student_id', DB::raw('MIN(student_fullname) as student_fullname'))
+                            ->groupBy('student_id')
+                            ->orderBy('student_fullname')
+                            ->pluck('student_fullname', 'student_id')
+                            ->toArray()
+                    ),
+
+                Tables\Filters\SelectFilter::make('status')
+                    ->options([
+                        'confirmed' => 'Confirmed',
+                        'canceled' => 'Canceled',
+                        'completed' => 'Completed',
+                    ]),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -72,7 +124,10 @@ class BookingManagementResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
-            ])->defaultSort('start_time');
+            ])
+            ->defaultSort('start_time')
+            ->filtersLayout(FiltersLayout::AboveContentCollapsible)
+            ->filtersFormColumns(6);
     }
 
     public static function getRelations(): array
