521681974435541cdf7702314e5c4ed9a4f6ab5f
denis.popov <denispopov2112@gmail.com>
2025-05-20 10:41:36 +0200

    Theme

--- Modules/Theme/resources/views/layouts/navigation.blade.php
+++ Modules/Theme/resources/views/layouts/navigation.blade.php
@@ -44,7 +44,7 @@
 
                         <x-slot name="content">
                             <x-dropdown-link :href="route('profile.dashboard')">
-                                <span class="flex items-center">
+                                <span class="flex items-center mt-2 text-base">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2 text-gray-600">
                                       <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                     </svg>
@@ -52,7 +52,7 @@
                                 </span>
                             </x-dropdown-link>
                             <x-dropdown-link :href="route('profile.account-information.edit')">
-                                <span class="flex items-center">
+                                <span class="flex items-center mt-2 text-base">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2 text-gray-600">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" />
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -62,7 +62,7 @@
                             </x-dropdown-link>
                             <hr class="my-3 border-t border-gray-300">
                             <x-dropdown-link :href="route('profile.account-information.edit')">
-                                <span class="flex items-center">
+                                <span class="flex items-center text-base">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2 text-gray-600">
                                       <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
                                     </svg>
@@ -70,7 +70,7 @@
                                 </span>
                             </x-dropdown-link>
                             <x-dropdown-link :href="route('blog')">
-                                <span class="flex items-center">
+                                <span class="flex items-center mt-2 text-base">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2 text-gray-600">
                                       <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
                                     </svg>
@@ -81,7 +81,7 @@
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
                                 <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
-                                    <span class="flex items-center">
+                                    <span class="flex items-center mb-2 text-base">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
                                         </svg>
--- Modules/User/routes/web.php
+++ Modules/User/routes/web.php
@@ -12,14 +12,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('user', UserController::class)->names('user');
 });
 
-Route::middleware('auth')->prefix('profile')->group(function () {
-    // Account Dashboard
-    Route::get('/', [UserController::class, 'index'])->name('profile.dashboard');
-
+Route::middleware('auth')->prefix('account')->group(function () {
+    // Profile Dashboard
+    Route::get('/profile', [UserController::class, 'index'])->name('profile.dashboard');
     // Account Information
-    Route::get('/account-information', [UserController::class, 'edit'])->name('profile.account-information.edit');
-    Route::patch('/account-information', [UserController::class, 'update'])->name('profile.account-information.update');
-    Route::delete('/account-information', [UserController::class, 'destroy'])->name('profile.account-information.destroy');
+    Route::get('/edit', [UserController::class, 'edit'])->name('profile.account-information.edit');
+    Route::patch('/edit', [UserController::class, 'update'])->name('profile.account-information.update');
+    Route::delete('/edit', [UserController::class, 'destroy'])->name('profile.account-information.destroy');
 
     Route::get('/my-orders', [OrderController::class, 'index'])->name('profile.orders.index');
     Route::post('/my-orders', [OrderController::class, 'store'])->name('profile.orders.store');
